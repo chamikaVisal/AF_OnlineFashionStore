96e86b35b97c876acc8c98d4239c7c368bfc8ef0
chamikaVisal <chamikaperera456@gmail.com>
2020-05-21 19:30:22 +0530

    Alerts and prodroct order request/response added

---
+++ Backend/src/main/java/com/giga/FashionStore/request/OrderProductRequest.java
@@ -0,0 +1,22 @@
+package com.giga.FashionStore.request;
+
+public class OrderProductRequest {
+    private String prod_id;
+    private int quantity;
+
+    public String getProd_id() {
+        return prod_id;
+    }
+
+    public void setProd_id(String prod_id) {
+        this.prod_id = prod_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
